508b78f061d2c9b5e5feab142c926a8a1291ab06
Salman Ul Haq <salman@programmerfish.com>
2015-01-28 12:57:00 +0500

    minimal version

--- test/opencl_version_check.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef BOOST_COMPUTE_TEST_OPENCL_VERSION_CHECK_HPP
-#define BOOST_COMPUTE_TEST_OPENCL_VERSION_CHECK_HPP
-
-#define REQUIRES_OPENCL_VERSION(major, minor) \
-    if (!device.check_version(major, minor)) return
-
-#endif
